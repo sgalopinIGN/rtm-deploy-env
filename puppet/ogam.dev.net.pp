619105612bc9fb5f2c59ecfe1bcaa4cb6649f362
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-03-30 13:59:44 +0200

    adding mailer_ip_address

--- puppet/ogam.dev.net.pp
+++ puppet/ogam.dev.net.pp
@@ -3,6 +3,7 @@ node 'ogam.dev.net' {
         vhost_servername => 'ogam.dev.net',
         admin_ip_address => '192.168.50.1',
         host_ip_address => '192.168.50.18',
+		mailer_ip_address => '192.134.133.226', # smtp1.ign.fr
         pg_user => 'postgres',
         pg_password => 'postgres',
         app_name => 'ogam'
