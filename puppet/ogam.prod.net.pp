720eb170d4584f28f8a214cfdca9c0d572e106e5
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-04-12 10:00:36 +0200

    Merge from development

--- puppet/ogam.prod.net.pp
+++ puppet/ogam.prod.net.pp
@@ -1,10 +1,11 @@
 node 'ogam.prod.net' {
-	class { 'ogam':
-		vhost_servername => 'ogam.prod.net',
-		admin_ip_address => '192.168.50.1',
-		host_ip_address => '192.168.50.19',
-		pg_user => 'postgres',
-		pg_password => 'postgres',
-		app_name => 'ogam'
-	}
+    class { 'ogam':
+        vhost_servername => 'ogam.prod.net',
+        admin_ip_address => '192.168.50.1',
+        host_ip_address => '192.168.50.19',
+        mailer_ip_address => '192.134.133.226', # smtp1.ign.fr
+        pg_user => 'postgres',
+        pg_password => 'postgres',
+        app_name => 'ogam'
+    }
 }
