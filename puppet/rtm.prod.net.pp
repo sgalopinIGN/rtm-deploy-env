10e6d52b2a113ed95142d349479187037dba2adf
sgalopinIGN <sylvain.galopin@ign.fr>
2019-11-22 14:11:49 +0100

    Adding extra-parameters

--- puppet/rtm.prod.net.pp
+++ puppet/rtm.prod.net.pp
@@ -5,6 +5,10 @@ node 'rtm.prod.net' {
 		admin_ip_address => '192.168.50.1',
 		host_ip_address => '192.168.50.22',
 		pg_user => 'postgres',
-		pg_password => 'postgres'
+		pg_password => 'postgres',
+		pg_db_user => 'rtm',
+		pg_db_password =>  'rtm',
+		proxy_host => 'proxy.ign.fr',
+		proxy_port => '3128'
 	}
 }
